114f2a4531278bb005a52d237ed25d23b78c1304
Sara Zapata <sarazapatasaldarriaga@gmail.com>
2025-08-15 16:10:59 -0500

    Cambio en el corss

--- src/main/java/com/commercial_manager/controllers/SupplierController.java
+++ src/main/java/com/commercial_manager/controllers/SupplierController.java
@@ -12,7 +12,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/supplier")
 public class SupplierController {
