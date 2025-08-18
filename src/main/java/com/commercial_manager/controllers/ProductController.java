977622726c4dbfbe157b8113719d5a9ea2f530b2
cesara.montoya1 <cesara.montoya1@udea.edu.co>
2025-08-17 23:11:05 -0500

    bugfix (cors): fixed cors overriding

--- src/main/java/com/commercial_manager/controllers/ProductController.java
+++ src/main/java/com/commercial_manager/controllers/ProductController.java
@@ -13,7 +13,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
